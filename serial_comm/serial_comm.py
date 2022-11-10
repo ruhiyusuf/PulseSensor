1153d39b73b1842fe8e5d4dae7279c09775fc5a2
ruhiyusuf <ruhi.yusuf@gmail.com>
2022-11-10 00:01:04 -1000

    wip

--- serial_comm/serial_comm.py
+++ serial_comm/serial_comm.py
@@ -2,9 +2,15 @@
 import serial
 import random
 import socket
+import sys
 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-server_socket.bind(('', 12000))
+# Create a TCP/IP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# Bind the socket to the port
+server_address = ('localhost', 10000)
+print >>sys.stderr, 'starting up on %s port %s' % server_address
+sock.bind(server_address)
 
 if __name__ == '__main__':
 	ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
@@ -13,6 +19,13 @@ if __name__ == '__main__':
 		if ser.in_waiting > 0:
 			line = ser.readline().decode('utf-8').rstrip()
 			print(line)
-			message, address = server_socket.recvfrom(1024)
-			message = message.upper()
-			server_socket.sendto(message, address)
+			print >>sys.stderr, '\nwaiting to receive message'
+    data, address = sock.recvfrom(4096)
+
+		print >>sys.stderr, 'received %s bytes from %s' % (len(data), address)
+		print >>sys.stderr, data
+
+		if data:
+			sent = sock.sendto(data, address)
+			print >>sys.stderr, 'sent %s bytes back to %s' % (sent, address)
+
