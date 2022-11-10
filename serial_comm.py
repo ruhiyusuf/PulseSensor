7e1695740a31d5e946d8c000d36696975378a10f
ruhiyusuf <ruhi.yusuf@gmail.com>
2022-11-09 23:48:05 -1000

    reads from serial port + broadcast udp as servergit add .

--- serial_comm.py
+++ serial_comm.py
@@ -1,9 +1,21 @@
 #!/usr/bin/env python3
 import serial
+import random
+import time
+import socket
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM,
+socket.IPPROTO_UDP)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+server_socket.settimeout(0.2)
+
 if __name__ == '__main__':
-    ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
-    ser.reset_input_buffer()
-    while True:
-        if ser.in_waiting > 0:
-            line = ser.readline().decode('utf-8').rstrip()
-            print(line)
+	ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+	ser.reset_input_buffer()
+	while True:
+		if ser.in_waiting > 0:
+			line = ser.readline().decode('utf-8').rstrip()
+			print(line)
+			server_socket.sendto(line.encode('ascii'), ('<broadcast>', 37020))
+			time.sleep(1)
