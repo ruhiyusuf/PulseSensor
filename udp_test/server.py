3afd60236b86c3b0e58497b03ec06e68b680050c
ruhiyusuf <ruhi.yusuf@gmail.com>
2022-11-13 19:36:23 -1000

    added server code - working

--- udp_test/server.py
+++ udp_test/server.py
@@ -1,18 +1,22 @@
+#!/usr/bin/env python3
+import serial
 import random
-import socket
 import time
+import socket
 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-# server_socket.bind(('', 12000))
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM,
+socket.IPPROTO_UDP)
 server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-#   server_socket.setsockopt(sockfd, SOL_SOCKET, SO_BROADCAST
-#		broadcastEnable, sizeof(broadcastEnable))
 server_socket.settimeout(0.2)
-while True:
-    # message, address = server_socket.recvfrom(1024)
-    # message = message.upper()
-    message = b"hello!!"
-    server_socket.sendto(message, ('<broadcast>', 37020))
-    # server_socket.sendto(message, address)
-    time.sleep(1)
+
+if __name__ == '__main__':
+	ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+	ser.reset_input_buffer()
+
+	while True:
+		if ser.in_waiting > 0:
+			line = ser.readline().decode('utf-8').rstrip()
+			print(line)
+			server_socket.sendto(line.encode('ascii'), ('<broadcast>', 37020))
+			time.sleep(1)
